4b48890ae72ead0c284779d079ede581479e7073
PUSHRSP <pushrsp@gmail.com>
2023-02-05 16:28:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/practicespringupload/PracticeSpringUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicespringupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringUploadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicespringupload/PracticeSpringUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicespringupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
